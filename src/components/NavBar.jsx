e42b77c245a0384a85d4149f14020ba4cd24184e
Akash Patil <akashpatil4659@gmail.com>
2025-01-21 13:53:23 +0530

    add daisyui package and code cleanup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+export default function NavBar() {
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control"></div>
+        <div className="dropdown dropdown-end mr-4">
+          <div
+            tabIndex={0}
+            role="button"
+            className="avatar btn btn-circle btn-ghost"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-300 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

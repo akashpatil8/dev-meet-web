bf74972942c7c85fa0370e640964c10b4db6ac52
Akash Patil <akashpatil4659@gmail.com>
2025-01-24 22:47:59 +0530

    implement sign up functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,9 +6,14 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 export default function Login() {
-  const [email, setEmail] = useState("akash@gmail.com");
-  const [password, setPassword] = useState("Akash@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -29,61 +34,93 @@ export default function Login() {
     }
   }
 
+  async function handleSignUp() {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          email,
+          password,
+          firstName,
+          lastName,
+        },
+        { withCredentials: true },
+      );
+      dispatch(addUser(res.data.user));
+      navigate("/profile/view");
+    } catch (error) {
+      setError(error?.response?.data);
+    }
+  }
+
   return (
-    <main className="flex-1">
-      <div className="card mx-auto mt-20 w-96 bg-base-200 shadow-xl">
+    <main className="flex flex-1 items-center justify-center p-4">
+      <div className="card mx-auto w-96 bg-base-200 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Login</h2>
-          <label className="input input-bordered my-4 flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70"
-            >
-              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-            </svg>
+
+          {isSignUp && (
+            <>
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">First name</span>
+                </div>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </label>
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Last name</span>
+                </div>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </label>
+            </>
+          )}
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Email</span>
+            </div>
             <input
-              value={email}
               type="text"
-              className="grow"
-              placeholder="Email"
+              value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="input input-bordered w-full max-w-xs"
             />
           </label>
-          <label className="input input-bordered mb-4 flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
             <input
+              type="password"
               value={password}
-              className="grow"
-              placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
+              className="input input-bordered w-full max-w-xs"
             />
           </label>
           <p className="text-red-400">{error}</p>
-          <div className="card-actions justify-end">
+          <button
+            onClick={() => setIsSignUp((val) => !val)}
+            className="mb-3 cursor-pointer text-center text-sm hover:text-white"
+          >
+            {isSignUp ? "Already a user? Login" : "New user? Sign Up"}
+          </button>
+          <div className="card-actions justify-center">
             <button
-              className="btn btn-ghost"
               onClick={() => {
-                setEmail("");
-                setPassword("");
+                isSignUp ? handleSignUp() : handleLogin();
               }}
+              className="btn btn-primary"
             >
-              Clear
-            </button>
-            <button onClick={handleLogin} className="btn btn-primary">
               Login
             </button>
           </div>
